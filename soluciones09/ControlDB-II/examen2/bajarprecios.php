8a4ce50fd45785be174fc33ec910c39a6b9af645
Alberto <alberto.lopez@iestetuan.es>
2022-01-13 10:13:23 +0100

    control BD mejorado

--- soluciones09/ControlDB-II/examen2/bajarprecios.php
+++ soluciones09/ControlDB-II/examen2/bajarprecios.php
@@ -2,12 +2,20 @@
 include_once 'AccesoDatos.php';
 include_once 'Producto.php';
 
+
+
 $ac = AccesoDatos::initModelo();
 
 
 if  (isset($_POST['orden']) && isset($_POST["tproductos"])){
+
+	if ( isset($_COOKIE["precioscambiados"])){
+		$msg=" Solo se puede hacer una rebaja al día ";
+	} else {
+    setcookie("precioscambiados","si",time()+3600*24);
     $tProductoNoActualizar=$_POST["tproductos"];
     $ac->actualizarPrecios($tProductoNoActualizar);
+	}
 }
 // Obtiene la lista de productos actual 
 $tproductos = $ac->obtenerListaProductos();
@@ -20,11 +28,14 @@ $tproductos = $ac->obtenerListaProductos();
 <link href="default.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-	<div id="container" style="width: 500px;">
+	<div id="container" style="width: 600px;">
 		<div id="header">
 			<h1>BAJAR PRECIOS PRODUCTOS SIN PEDIDOS</h1>
 		</div>
 		<div id="content">
+		<?php if (isset($msg)): ?>
+		  <h2> <?= $msg ?> </h2>
+		<?php else: ?>
 		<form name='entrada' method="post" >
 			    <table border=1>
 			    <tr>
@@ -48,6 +59,7 @@ $tproductos = $ac->obtenerListaProductos();
 				<input type="submit" name="orden" value="ACTUALIZAR">
 			</form>
 		</div>
+		<?php endif; ?>
 	</div>
 </body>
 </html>
